0fa0344c43669c105637b9fa93862705260f093d
bowlingball3525 <51011895+bowlingball3525@users.noreply.github.com>
2023-09-09 12:00:21 -0500

    Update ToolSpam.lua

--- ToolSpam.lua
+++ ToolSpam.lua
@@ -1,2 +1,20 @@
 wait(5)
 warn("bot loaded")
+wait(1)
+task.spawn(
+    function()
+        while true do
+            wait()
+        for _, x in next, game.Players.LocalPlayer.Backpack:GetChildren() do
+            if x:IsA("Tool") then
+                x.Parent = game.Players.LocalPlayer.Character
+                x.Parent = workspace
+            end
+        end
+        game.ReplicatedStorage.GiveCat:FireServer()
+        game.ReplicatedStorage.GiveCat:FireServer()
+        game.ReplicatedStorage.GiveBox:FireServer()
+        game.ReplicatedStorage.GiveBox:FireServer()
+    end
+end
+)
