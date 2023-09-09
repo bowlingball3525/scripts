f3c1b21c6f55afcd3e686e4973093b0704b2e552
bowlingball3525 <51011895+bowlingball3525@users.noreply.github.com>
2023-09-09 12:00:10 -0500

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -52,24 +52,6 @@ coroutine.wrap(function()
     end
 end)()
 
-task.spawn(
-    function()
-        while true do
-            wait()
-        for _, x in next, game.Players.LocalPlayer.Backpack:GetChildren() do
-            if x:IsA("Tool") then
-                x.Parent = game.Players.LocalPlayer.Character
-                x.Parent = workspace
-            end
-        end
-        game.ReplicatedStorage.GiveCat:FireServer()
-        game.ReplicatedStorage.GiveCat:FireServer()
-        game.ReplicatedStorage.GiveBox:FireServer()
-        game.ReplicatedStorage.GiveBox:FireServer()
-    end
-end
-)
-
 spawn(
     function()
         while true do
