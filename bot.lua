a0c5a0ffbb49cd6017e1ba1733b9540d8b03182f
bowlingball3525 <51011895+bowlingball3525@users.noreply.github.com>
2023-09-08 23:04:11 -0500

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -1,3 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/v9h/dhrpvc/main/anticrash.lua", true))()
+
 wait(5)
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/v9h/dhrpvc/main/anticrash.lua", true))()
