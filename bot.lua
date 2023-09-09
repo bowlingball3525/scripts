dea7930f41771484c8c529653337927c5c62956b
bowlingball3525 <51011895+bowlingball3525@users.noreply.github.com>
2023-09-09 11:56:07 -0500

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -2,11 +2,6 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/v9h/dhrpvc/main/antic
 wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/v9h/dhrpvc/main/anticrash.lua", true))()
 
-tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(0.5, Enum.EasingStyle.Linear)
-tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-77.74261474609375, 139.6412353515625, 52.56494903564453)}):Play()
-
-wait(1)
-
 local function ServerHop()
     function shop()
         pcall(function()
